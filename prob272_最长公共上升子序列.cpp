d650b29808ffe586610b1741995ec494e5403fd3
cheng.zhaoxi <cheng.zhaoxi@immomo.com>
2020-02-05 18:39:48 +0800

    20200205

--- prob272_最长公共上升子序列.cpp
+++ prob272_最长公共上升子序列.cpp
@@ -31,30 +31,96 @@ void solve(const vector<int> &A, const vector<int> &B)
     // 状态转移
     // dp[i][j] = 0         (A[i] != B[j]时)
     //          = k < i, l < j 的某个 k, l 处, 且要满足 A[k] < A[i], B[l] < B[j]
-    //            dp[k][l] 取到 max (A[i] == B[j]时)
+    //            dp[k][l] 取到 max ，然后再加1(A[i] == B[j]时)
     int N = A.size();
-    vector<vector<int> > dp(N, vector<int>(N, 0));
-    for(int i = 0; i < N; ++i)
-        for(int j = 0; j < N; ++j)
+    vector<vector<int> > dp(N + 1, vector<int>(N + 1, 0));
+    int ans = 0;
+    for(int i = 1; i < N; ++i)
+        for(int j = 1; j < N; ++j)
             if(A[i] == B[j])
-                for(int k = 0; k < i; ++k)
-                    for(int l = 0; l < j; ++l)
+            {
+                for(int k = 1; k < i; ++k)
+                    for(int l = 1; l < j; ++l)
                         if(A[k] < A[i] && B[l] < B[j])
                             dp[i][j] = max(dp[i][j], dp[k][l]);
-    cout << dp[N - 1][N - 1];
+                dp[i][j] += 1;
+                ans = max(ans, dp[i][j]);
+            }
+    cout << ans << endl;
     return;
 }
+
+void solve_optim_1(const vector<int> &A, const vector<int> &B)
+{
+    // 求 max dp[k][l] 时候，对 max 有贡献的 k,l 一定是匹配的(A[k] == B[l])
+    // 所以 max 的条件中 A[k]<A[i] 和 B[l]<B[j] 只要满足了一个，另一个一定满足
+    //
+    // 状态定义
+    // dp[i][j] := 考虑到 A[i],B[j],且选了 B[j] 的 LCIS 的长度
+    // 状态转移
+    // dp[i][j] = dp[i - 1]dp[j] (A[i] != B[j]时)
+    //            因为定义是选了 j 因此，一定有某个 A[k] = B[j] 该信息在 dp[i - 1][j]中 -> i 这个维度是前缀信息
+    //          = l < j 且 B[l] < B[j]，
+    //            dp[i - 1][l] 取到的最大值再加 1. (A[i] == B[j]时)
+    //
+    // 可以滚动数组优化空间
+    int N = A.size();
+    vector<int> dp(N + 1, 0);
+    for(int i = 1; i < N; ++i)
+        for(int j = 1; j < N; ++j)
+        {
+            if(A[i] == B[j])
+            {
+                int mv = 0;
+                for(int l = 1; l < j; ++l)
+                    if(B[l] < B[j])
+                        mv = max(mv, dp[l]);
+                dp[j] = mv + 1;
+            }
+        }
+
+    int ans = 0;
+    for(int i = 1; i < N; ++i)
+        ans = max(ans, dp[i]);
+    cout << ans << endl;
+    return;
+}
+
+void solve_optim_2(const vector<int> &A, const vector<int> &B)
+{
+    // optim_1 的基础上对代码做等价变形
+    int N = A.size();
+    vector<int> dp(N + 1, 0);
+    for(int i = 1; i < N; ++i)
+    {
+        int mv = 0;
+        for(int j = 1; j < N; ++j)
+        {
+            if(B[j] < A[i])
+                mv = max(mv, dp[j]);
+            if(A[i] == B[j])
+                dp[j] = mv + 1;
+        }
+    }
+
+    int ans = 0;
+    for(int i = 1; i < N; ++i)
+        ans = max(ans, dp[i]);
+    cout << ans << endl;
+    return;
+}
+
 
 int main()
 {
     int N;
     cin >> N;
-    vector<int> A(N, 0);
-    vector<int> B(N, 0);
-    for(int i = 0; i < N; ++i)
+    vector<int> A(N + 1, 0);
+    vector<int> B(N + 1, 0);
+    for(int i = 1; i <= N; ++i)
         cin >> A[i];
-    for(int i = 0; i < N; ++i)
+    for(int i = 1; i <= N; ++i)
         cin >> B[i];
-    solve(A, B);
+    solve_optim_1(A, B);
     return 0;
 }
