77eb6fe48be1baa7baa08efda29189cd85137e4c
fenneldumplings <2585811498@qq.com>
2022-03-15 21:19:52 +0800

    2022-03-15

--- prob291_蒙德里安的梦想.cpp
+++ prob291_蒙德里安的梦想.cpp
@@ -3,44 +3,45 @@
 
 using namespace std;
 
-bool check(int x)
+bool check(const int x, const int M)
 {
-    // x 二进制的连续 1 长度均为偶数
-    while(x > 0)
+    // x 二进制的连续 0 长度均为偶数
+    int i = M - 1;
+    while(i >= 0)
     {
         int len = 0;
-        while(x > 0 && (x & 1) == 1)
+        while(i >= 0 && (x >> i & 1) == 0)
         {
             ++len;
-            x >>= 1;
+            --i;
         }
         if(len & 1)
             return false;
-        if(x == 0)
-            break;
-        x >>= 1;
+        while(i >= 0 && (x >> i & 1) == 1)
+            --i;
     }
     return true;
 }
 
 int main()
 {
+    using ll = long long;
     const int MAX_M = 11;
     int N = 11;
-    vector<vector<vector<int>>> dp_all(MAX_M + 1);
-    for(int M = 1; M <= 11; ++M)
+    vector<vector<vector<ll>>> dp_all(MAX_M + 1);
+    for(int M = 1; M <= MAX_M; ++M)
     {
-        vector<vector<int>> dp(N + 1, vector<int>(1 << M));
+        vector<vector<ll>> dp(N + 1, vector<ll>(1 << M));
         dp[0][0] = 1;
         for(int i = 1; i <= N; ++i)
         {
-            for(int s = 0; s < (1 << 11) - 1; ++s)
+            for(int s = 0; s < (1 << M); ++s)
             {
-                for(int t = 0; t < (1 << 11) - 1; ++t)
+                for(int t = 0; t < (1 << M); ++t)
                 {
                     if((s & t) != 0)
                         continue;
-                    if(!check(s | t))
+                    if(!check(s | t, M))
                         continue;
                     dp[i][s] += dp[i - 1][t];
                 }
