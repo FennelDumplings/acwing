a7989b6378ba83b85e4aa9be7bd039ee0bbd13eb
fenneldumplings <2585811498@qq.com>
2022-03-21 01:28:53 +0800

    2022-03-21

--- prob113_特殊排序.cpp
+++ prob113_特殊排序.cpp
@@ -1,7 +1,7 @@
 #include <algorithm>
 #include <vector>
-#include <list>
 
+using namespace std;
 
 // Forward declaration of compare API.
 // bool compare(int a, int b);
@@ -10,14 +10,28 @@
 class Solution {
 public:
     vector<int> specialSort(int N) {
-        list<int> l;
-        l.push_back(1);
-        vector<list<int>::iterator> iters;
-        iters.push_back(l.begin());
+        vector<int> vec;
+        vec.push_back(1);
         for(int i = 2; i <= N; ++i)
         {
-            //
+            int left = 0, right = vec.size();
+            while(left < right)
+            {
+                int mid = (left + right) / 2;
+                int j = vec[mid];
+                if(compare(i, j))
+                {
+                    // i < j
+                    right = mid;
+                }
+                else
+                {
+                    // j < i
+                    left = mid + 1;
+                }
+            }
+            vec.insert(vec.begin() + left, i);
         }
-
+        return vec;
     }
 };
